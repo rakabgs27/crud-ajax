21bc22a4f83b0863cdba2790700c36ed6abac2c5
rakabgs27 <rakabgs2000@gmail.com>
2023-08-02 22:41:35 +0700

    Fixing Changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,42 +14,6 @@ class LoginController extends Controller
         return view('login');
     }
 
-    // public function login(Request $request)
-    // {
-    //     $credentials = $request->validate([
-    //         'email' => 'required|email',
-    //         'password' => 'required',
-    //     ]);
-
-    //     if (Auth::attempt($credentials)) {
-    //         $response = Http::post('api/auth/login', $credentials);
-
-    //         if ($response->ok()) {
-    //             $token = $response['token'];
-
-    //             // Lakukan sesuatu dengan token (misalnya, simpan di session atau cookie)
-    //             // ...
-
-    //             $request->session()->regenerate();
-
-    //             return redirect()->intended('/products');
-    //         } else {
-    //             $message = $response['message'];
-
-    //             // Lakukan sesuatu dengan pesan error dari API (misalnya, tampilkan pesan kesalahan ke pengguna)
-    //             // ...
-
-    //             return back()->withErrors([
-    //                 'email' => $message,
-    //             ]);
-    //         }
-    //     }
-
-    //     return back()->withErrors([
-    //         'email' => 'The provided credentials do not match our records.',
-    //     ]);
-    // }
-
     public function login(Request $request)
 {
     $credentials = $request->validate([

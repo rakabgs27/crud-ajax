89c94552baffd7131d5c64c496cfcf403ab2f457
rakabgs27 <rakabgs2000@gmail.com>
2023-08-11 10:46:55 +0700

    Laravel Mix

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,30 +15,30 @@ class LoginController extends Controller
     }
 
     public function login(Request $request)
-{
-    $credentials = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    $apiResponse = AuthController::getApiToken($credentials['email'], $credentials['password']);
-
-    if ($apiResponse['status']) {
-        $token = $apiResponse['data']['token'];
-
-        Session::put('api_token', $token);
-
-        $request->session()->regenerate();
-
-        return redirect()->intended('/products');
-    } else {
-        $message = $apiResponse['message'];
-
-        return back()->withErrors([
-            'email' => $message,
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
+
+        $apiResponse = AuthController::getApiToken($credentials['email'], $credentials['password']);
+
+        if ($apiResponse['status']) {
+            $token = $apiResponse['data']['token'];
+
+            Session::put('api_token', $token);
+
+            $request->session()->regenerate();
+
+            return redirect()->intended('/products');
+        } else {
+            $message = $apiResponse['message'];
+
+            return back()->withErrors([
+                'email' => $message,
+            ]);
+        }
     }
-}
 
     public function logout(Request $request)
     {
